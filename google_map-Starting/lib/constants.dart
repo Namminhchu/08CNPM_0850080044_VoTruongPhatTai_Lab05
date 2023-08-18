7fa118b2738772bbb7085eaba02a4a31b0c4745d
nguyen-hoang-nam123 <80116172+nguyen-hoang-nam123@users.noreply.github.com>
2023-08-18 16:47:23 +0700

    first commit

---
+++ google_map-Starting/lib/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyDdw9pUbh9YKhDWxIP2bT-WqK9ogeu-5h8";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
